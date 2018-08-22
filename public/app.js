c6d026dd748ce5fe3fce334a67104a1407ac8148
Valéria Barros <vb.valeriabarros@gmail.com>
2018-08-22 18:01:24 -0300

    Setting style

--- public/app.js
+++ public/app.js
@@ -1,18 +1,15 @@
 var app = (function () {
 
   function init() {
-    document.getElementById('wordCount').addEventListener('submit', countWords);
+    document.getElementById('word-count-form').addEventListener('submit', countWords);
   }
 
   function countWords(event) {
     event.preventDefault();
-    Countable.count(document.getElementById('words'), counter =>  {
-      document.getElementById('numberWords').innerText = "Your text has " + counter.words + " words!";
+    Countable.count(document.getElementById('word-count-text'), counter => {
+      document.getElementById('counter').innerText = "Your text has " + counter.words + " words!";
     });
   }
-
-  return {
-    init: init
-  };
+  return { init };
 })();
 app.init();
--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
-
 app.listen(port, () => {
   console.log('listening on %d', port);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
 });
